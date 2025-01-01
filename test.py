7f97f7c31f61d29467d80b39eebedd76764be1ea
Nhan <pducnhan70@gmail.com>
2025-01-01 23:38:29 +0700

    title

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("de");
+message="Hello world"
+print(message);
+# print(mesage);
+
+anewmessage=("okok");
+print(anewmessage);
+anewmessage=("nana");
+print(anewmessage);
+name="ada lovelave"
+print(name.title());
